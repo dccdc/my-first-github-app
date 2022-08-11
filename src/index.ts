f8b89ffd04e282e3d645ee15af9e61324695d354
CongcongDeng <congcong.deng@thoughtworks.com>
2022-08-11 17:36:26 +0800

    configure app

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,36 @@ import { Probot } from "probot";
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Thanks for opening this issue! P:",
     });
     await context.octokit.issues.createComment(issueComment);
   });
+
+  // app.on("issue_comment.created", async (context) => {
+  //   const issueComment = context.issue({
+  //     body: "Welcome for create a comment!",
+  //   });
+  //   await context.octokit.issues.createComment(issueComment);
+  // });
+
+  // app.on("pull_request", async (context) => {
+  //   const pr = context.pullRequest({
+  //     title: "[FE] hello world",
+  //     body: "test content 1",
+  //   });
+  //   await context.octokit.pulls.update(pr);
+  // });
+
+  app.on("pull_request.opened", async (context) => {
+    app.log.info(context);
+    app.log.info("666");
+    const pr = context.pullRequest({
+      title: "[FE] hello world",
+      body: "test content 2",
+    });
+    await context.octokit.pulls.update(pr);
+  });
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
