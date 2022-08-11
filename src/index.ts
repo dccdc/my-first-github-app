cb1010203333653e8c68ee6371ca55dd67321885
CongcongDeng <congcong.deng@thoughtworks.com>
2022-08-11 21:04:15 +0800

    add prefix feature

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,22 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue! P:",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-
-  // app.on("issue_comment.created", async (context) => {
-  //   const issueComment = context.issue({
-  //     body: "Welcome for create a comment!",
-  //   });
-  //   await context.octokit.issues.createComment(issueComment);
-  // });
-
-  // app.on("pull_request", async (context) => {
-  //   const pr = context.pullRequest({
-  //     title: "[FE] hello world",
-  //     body: "test content 1",
-  //   });
-  //   await context.octokit.pulls.update(pr);
-  // });
-
   app.on("pull_request.opened", async (context) => {
-    app.log.info(context);
-    app.log.info("666");
+    // app.log.info(context);
+    const {
+      pull_request: { title },
+      repository: { name },
+    } = context.payload;
+
+    let prefix = "[FE]";
+    if (name.toLocaleLowerCase().indexOf("backend") > -1) {
+      prefix = "[BE]";
+    }
+
     const pr = context.pullRequest({
-      title: "[FE] hello world",
-      body: "test content 2",
+      title: `${prefix} ${title}`,
     });
+
     await context.octokit.pulls.update(pr);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
